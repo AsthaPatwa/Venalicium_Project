0b21c03375ec7c639e77e9f5bfd9fb8b53e9f2a4
unknown <kushwahanjali037@gmail.com>
2022-02-17 21:59:46 +0530

    footer done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 import Cpage1 from "../contactPages/Cpage1";
 import Cpage2 from "../contactPages/Cpage2";
 import Cpage3 from "../contactPages/Cpage3";
+import Footer from "../components/Footer"
+
 
 const Contact = () => {
   return (
